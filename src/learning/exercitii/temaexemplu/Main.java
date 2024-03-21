0823781860f734bd522cf548a112ed14b746e239
Claudiu Haidu <claudiu.haidu@globant.com>
2024-03-21 18:41:07 +0200

    tema1 exmeplu

---
+++ src/learning/exercitii/temaexemplu/Main.java
@@ -0,0 +1,7 @@
+package learning.exercitii.temaexemplu;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/learning/exercitii/temaexemplu/Tema1.java
@@ -0,0 +1,5 @@
+package learning.exercitii.temaexemplu;
+
+public class Tema1 {
+    public String param1;
+}
